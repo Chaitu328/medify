90c7bfc8c59521a27e66b8b45e4f901700c2abaf
chaitu328 <konakallachaitanya@gmail.com>
2024-08-10 14:56:26 +0530

    searchSection component is done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,40 @@
-function SearchBar(){
+import React from 'react';
+import { IoIosSearch } from "react-icons/io";
+import styles from './SearchBar.module.css'; // Import CSS module
 
+function SearchBar() {
+    return (
+        <div className={styles.searchContainer}>
+            <form className={styles.formWrapper}>
+                <div className={styles.inputWrapper}>
+                    <input
+                        type="text"
+                        id='state'
+                        name="state"
+                        placeholder="State"
+                        className={styles.searchInput}
+                    />
+                    <IoIosSearch className={styles.searchIcon} />
+                </div>
+
+                <div className={styles.inputWrapper}>
+                    <input
+                        type="text"
+                        id='city'
+                        name="city"
+                        placeholder="City"
+                        className={styles.searchInput}
+                    />
+                    <IoIosSearch className={styles.searchIcon} />
+                </div>
+
+                <button type='submit'>
+                    <IoIosSearch className={styles.searchIcon} />
+                    Search
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar;
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,14 +1,49 @@
-import NavBar from "../../Navabar/Navbar"
-import HeroSection from "../../HeroSection/HeroSection"
-import styles from "./HomePage.module.css"
+import NavBar from "../../Navabar/Navbar";
+import HeroSection from "../../HeroSection/HeroSection";
+import styles from "./HomePage.module.css";
+import SearchBar from "../../SearchBar/SearchBar";
+import doctorLogo from "../../assets/Doctos.png"
+import labsLogo from "../../assets/labs.png"
+import hospitalLogo from "../../assets/hospitals.png"
+import storeLogo from "../../assets/medical_store.png"
+import ambulanceLogo from "../../assets/ambulance.png"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function HomePage(){
+
+function HomePage() {
     return (
-        <div className={styles.home} >
-            <NavBar/>
-            <HeroSection/>
+        <div className={styles.home}>
+            <NavBar />
+            <HeroSection />
+            <div className={styles.section}>
+                <div className={styles.searchWrapper}>
+                    <SearchBar />
+                    <h3 className={styles.searchText}>You may be looking for</h3>
+                    <div className="card-deck" style={{
+                        width: '90%',
+                        height: '90%',
+                    }}>
+                        <div className="card">
+                            <img src={doctorLogo} alt="logo is not found" />
+                        </div>
+                        <div className="card">
+                            <img src={labsLogo} alt="logo is not found" />
+                        </div>
+                        <div className="card">
+                            <img src={hospitalLogo} alt="logo is not found" />
+                        </div>
+                        <div className="card">
+                            <img src={storeLogo} alt="logo is not found" />
+                        </div>
+                        <div className="card">
+                            <img src={ambulanceLogo} alt="logo is not found" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
