227794cf9d1be4cdfd022e0eb9831e94c5c8ef5b
chaitu328 <konakallachaitanya@gmail.com>
2024-08-12 16:22:01 +0530

    navbar,searchbar for doctor page is done

---
+++ src/components/BookingBar/BookingBar.jsx
@@ -0,0 +1,11 @@
+import styles from "./BookingBar.module.css"
+
+function BookingBar(){
+    return(
+        <div className={styles.box}>
+            My Bookings
+        </div>
+    )
+}
+
+export default BookingBar
--- src/components/pages/DoctorPage/DoctorPage.jsx
+++ src/components/pages/DoctorPage/DoctorPage.jsx
@@ -1,9 +1,17 @@
 import NavBar from "../../Navabar/Navbar"
+import BookingBar from "../../BookingBar/BookingBar"
+import SearchBar from "../../SearchBar/SearchBar"
+import styles from "./DoctorPage.module.css"
 
 function DoctorPage(){
     return (
         <div>
            <NavBar/>
+           <BookingBar/>
+           <div className={styles.section }>
+            <SearchBar />
+           </div>
+          
         </div>
     )
 }
